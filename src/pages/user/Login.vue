c368f880716f59e1e407e784a781c2a56634d5d7
Simon Carr <simonjcarr@gmail.com>
2020-07-07 13:07:41 +0100

    formatted login and register pages

--- src/pages/user/Login.vue
+++ src/pages/user/Login.vue
@@ -1,52 +1,74 @@
 <template>
   <q-page class="row justify-center" padding>
     <div class="col-4">
-      <div class="text-h4">Login</div>
-      <div v-if="errors.length > 0" class="bg-red-2 rounded-borders q-pa-sm text-red-10">
-        <div v-for="(error, index) in errors" :key="index">{{error.message}}</div>
-      </div>
-      <q-form
-        @submit="onSubmit"
-        class="q-gutter-md"
-      >
-        <div>
-          <q-input class="full-width" v-model="email" type="email" label="Email" />
-        </div>
-        <div>
-          <q-input class="full-width" v-model="password" type="password" label="Password" />
-        </div>
-        <div>
-          <q-btn label="Submit" type="submit" color="primary"/>
-        </div>
-      </q-form>
+      <q-card class="bg-accent">
+        <q-card-section>
+          <q-card>
+            <q-card-section>
+              <div class="text-h4">Login</div>
+            </q-card-section>
+            <q-card-section>
+              <div
+                v-if="errors.length > 0"
+                class="bg-red-2 rounded-borders q-pa-sm text-red-10"
+              >
+                <div v-for="(error, index) in errors" :key="index">
+                  {{ error.message }}
+                </div>
+              </div>
+              <q-form @submit="onSubmit" class="q-gutter-md">
+                <div>
+                  <q-input
+                    class="full-width"
+                    v-model="email"
+                    type="email"
+                    label="Email"
+                  />
+                </div>
+                <div>
+                  <q-input
+                    class="full-width"
+                    v-model="password"
+                    type="password"
+                    label="Password"
+                  />
+                </div>
+                <div>
+                  <q-btn label="Submit" type="submit" color="primary" />
+                </div>
+              </q-form>
+            </q-card-section>
+          </q-card>
+        </q-card-section>
+      </q-card>
     </div>
-</q-page>
+  </q-page>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions } from "vuex";
 export default {
   data: () => {
     return {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
       errors: []
-    }
+    };
   },
   methods: {
-    ...mapActions('user', ['login']),
+    ...mapActions("user", ["login"]),
     onSubmit() {
       this.login({
         email: this.email,
         password: this.password
-      }).then((result)=>{
-        this.$router.push('/')
-      }).catch((err) => {})
+      })
+        .then(result => {
+          this.$router.push("/");
+        })
+        .catch(err => {});
     }
   }
-}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/pages/user/Register.vue
+++ src/pages/user/Register.vue
@@ -1,82 +1,115 @@
 <template>
-<q-page class="row justify-center" padding>
+  <q-page class="row justify-center" padding>
     <div class="col-4">
-      <div class="text-h4">Register</div>
-      <div v-if="errors.length > 0" class="bg-red-2 rounded-borders q-pa-sm text-red-10">
-        <div v-for="(error, index) in errors" :key="index">{{error.message}}</div>
-      </div>
-      <q-form
-        @submit="onSubmit"
-        class="q-gutter-md"
-      >
-        <div>
-          <q-input class="full-width" v-model="username" type="text" label="Username" />
-        </div>
-        <div>
-          <q-input class="full-width" v-model="email" type="email" label="Email" />
-        </div>
-        <div>
-          <q-input class="full-width" v-model="password" type="password" label="Password" />
-        </div>
-        <div>
-          <q-input class="full-width" v-model="confirm_password" type="password" label="Confirm Password" />
-        </div>
-        <div>
-          <q-btn label="Submit" type="submit" color="primary"/>
-        </div>
-      </q-form>
+      <q-card class="bg-accent">
+        <q-card-section>
+          <q-card class="my-card">
+            <q-card-section>
+              <div class="text-h4">Register</div>
+            </q-card-section>
+            <q-card-section>
+              <div
+                v-if="errors.length > 0"
+                class="bg-red-2 rounded-borders q-pa-sm text-red-10"
+              >
+                <div v-for="(error, index) in errors" :key="index">
+                  {{ error.message }}
+                </div>
+              </div>
+              <q-form @submit="onSubmit" class="q-gutter-md">
+                <div>
+                  <q-input
+                    class="full-width"
+                    v-model="username"
+                    type="text"
+                    label="Username"
+                  />
+                </div>
+                <div>
+                  <q-input
+                    class="full-width"
+                    v-model="email"
+                    type="email"
+                    label="Email"
+                  />
+                </div>
+                <div>
+                  <q-input
+                    class="full-width"
+                    v-model="password"
+                    type="password"
+                    label="Password"
+                  />
+                </div>
+                <div>
+                  <q-input
+                    class="full-width"
+                    v-model="confirm_password"
+                    type="password"
+                    label="Confirm Password"
+                  />
+                </div>
+                <div>
+                  <q-btn label="Submit" type="submit" color="primary" />
+                </div>
+              </q-form>
+            </q-card-section>
+          </q-card>
+        </q-card-section>
+      </q-card>
     </div>
-</q-page>
+  </q-page>
 </template>
 
 <script>
-import Vue from 'vue'
-import { mapMutations, mapState } from 'vuex'
-var vm = new Vue()
+import Vue from "vue";
+import { mapMutations, mapState } from "vuex";
+var vm = new Vue();
 export default {
   data: () => {
     return {
-      username: '',
-      email: '',
-      password: '',
-      confirm_password: '',
+      username: "",
+      email: "",
+      password: "",
+      confirm_password: "",
       errors: []
-    }
+    };
   },
   computed: {
-    ...mapState('user', ['token'])
+    ...mapState("user", ["token"])
   },
   methods: {
-    ...mapMutations('user', ['setToken', 'setUser']),
+    ...mapMutations("user", ["setToken", "setUser"]),
 
     onSubmit() {
-      this.errors = []
-      this.$axios.post('/user/register', {
-        username: this.username,
-        email: this.email,
-        password: this.password,
-        confirm_password: this.confirm_password
-      }).then(({ data }) => {
-        console.log(data)
-        this.setToken(data.token)
-        this.setUser(data.user)
-        this.$q.notify({
-          type: 'positive',
-          message: 'Your account has been created you were signed in'
+      this.errors = [];
+      this.$axios
+        .post("/user/register", {
+          username: this.username,
+          email: this.email,
+          password: this.password,
+          confirm_password: this.confirm_password
         })
-      }).catch((err) => {
-        this.errors = err.response.data
-      })
+        .then(({ data }) => {
+          console.log(data);
+          this.setToken(data.token);
+          this.setUser(data.user);
+          this.$q.notify({
+            type: "positive",
+            message: "Your account has been created you were signed in"
+          });
+        })
+        .catch(err => {
+          this.errors = err.response.data;
+        });
     }
   },
   mounted() {
-    if(this.token) {
-      this.$router.push('/')
+    if (this.token) {
+      this.$router.push("/");
     }
   }
-}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
