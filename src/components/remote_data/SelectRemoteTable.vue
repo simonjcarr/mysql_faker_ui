901a052845bd39c9c0d5ff26569ff0e678084998
Simon Carr <simonjcarr@gmail.com>
2020-07-17 15:37:52 +0100

    added filter to table select in map remotes

--- src/components/remote_data/SelectRemoteTable.vue
+++ src/components/remote_data/SelectRemoteTable.vue
@@ -1,42 +1,61 @@
 <template>
   <q-card class="my-card">
     <q-card-section>
-      <q-select dense v-model="selectedTable" :options="tableOptions" label="Select Table" filled />
+      <q-select
+        dense
+        use-input
+        input-debounce="0"
+        @input-value="setFilter"
+        v-model="selectedTable"
+        :options="tableOptions"
+        label="Select Table"
+        filled
+      />
     </q-card-section>
   </q-card>
 </template>
 
 <script>
-import { mapState, mapMutations, mapActions } from 'vuex'
+import { mapState, mapMutations, mapActions } from "vuex";
 export default {
   data: () => {
-    return{
-      selectedTable: null
-    }
+    return {
+      selectedTable: null,
+      tableFilter: ""
+    };
   },
-  watch:{
-    selectedTable(table){
+  watch: {
+    selectedTable(table) {
       // this.$emit('selected', table)
-      this.setSelectedTable(table.value)
-      this.getTableSchema()
+      this.setSelectedTable(table.value);
+      this.getTableSchema();
     }
   },
-  computed:{
-    ...mapState('remotedata', ['tables']),
+  computed: {
+    ...mapState("remotedata", ["tables"]),
     tableOptions() {
-      return _.sortBy(this.tables, (o) => { return o.TABLE_NAME}).map(table => {
-        return {label: table.TABLE_NAME, value: table.TABLE_NAME}
-      })
+      var tables = this.tables;
+      if (this.tableFilter !== "") {
+        tables = _.filter(tables, t => {
+          return t.TABLE_NAME.indexOf(this.tableFilter) !== -1;
+        });
+      }
+      console.log(tables)
+      return _.sortBy(tables, o => {
+        return o.TABLE_NAME;
+      }).map(table => {
+        return { label: table.TABLE_NAME, value: table.TABLE_NAME };
+      });
     }
   },
-  methods:{
-    ...mapMutations('remotedata', ['setSelectedTable']),
-    ...mapActions('remotedata', ['getTableSchema'])
+  methods: {
+    ...mapMutations("remotedata", ["setSelectedTable"]),
+    ...mapActions("remotedata", ["getTableSchema"]),
+    setFilter(value){
+      this.tableFilter = value
+    }
   }
-
-}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
